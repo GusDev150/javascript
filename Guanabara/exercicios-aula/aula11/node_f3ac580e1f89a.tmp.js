3ba56eb8a81d099c9417fb4baf81d120341d4377
GustavoPHY <guxxxxtavo.silva@gmail.com>
2025-12-15 11:43:23 -0300

    .

--- Guanabara/exercicios-aula/aula11/node_f3ac580e1f89a.tmp.js
+++ Guanabara/exercicios-aula/aula11/node_f3ac580e1f89a.tmp.js
@@ -2,5 +2,7 @@ var vel = 12
 console.log(`A velocidade do seu carro é ${vel}Km/h`)
 if (vel > 60) {
     console.log(`Você ultrapassou a velocidade permitida. MULTADO`)
+} else {
+    console.log(`Você está dentro da lei!`)
 }
 console.log(`Diriga sempre usando cinto de segurança`)
