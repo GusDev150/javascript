7c80666ccfd6224119b7f7067f4ace1483089e72
GustavoPHY <guxxxxtavo.silva@gmail.com>
2025-12-16 14:31:43 -0300

    Js ex009

---
+++ Matheus/ex009/js/script.js
@@ -0,0 +1,27 @@
+var obj = {
+    Nome: 'Gustavo',
+    Idade: 16,
+    EstadoCivil: 'Solteiro',
+    estaTrabalhando: false,
+}
+
+console.log(obj)
+
+console.log(typeof obj)
+
+console.log(obj.Nome)
+console.log(obj.Idade)
+console.log(obj.EstadoCivil)
+console.log(obj.estaTrabalhando)
+
+console.log(`O meu nome é ${obj.Nome}!`)
+
+obj.Nome = "Guilherme"
+
+console.log(obj.Nome)
+
+console.log(obj.Nome)
+
+obj.graduacao = true
+
+console.log(obj)
