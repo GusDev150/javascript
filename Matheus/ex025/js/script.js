121dbfbeb8889ddf73474b13bf17f8cfa1d50f02
GustavoPHY <guxxxxtavo.silva@gmail.com>
2026-01-01 18:12:30 -0300

    arrumando o sistema

---
+++ Matheus/ex025/js/script.js
@@ -0,0 +1,9 @@
+// Utilizando let e const podemos criar escopo até em instruções como if
+// Deixando o código mais confiável
+// Separando cada bloco em um escopo
+
+let x = 5 // var x = 5
+
+x = 12
+
+console.log(x)
